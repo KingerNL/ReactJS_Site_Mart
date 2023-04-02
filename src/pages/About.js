fe27c6990525ba675658b30cacf281049d46da51
Mart <mart.veldkamp@gmail.com>
2023-04-02 15:54:15 +0200

    Added Iphone support

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,13 @@
 export default function About() {
 
     return (
-    <div className="home_about">
-        <div className="about_text">
-            <h2> Me, Myself &amp; I</h2>
-            <p> Although I am studying mechatronics &amp; robotics, I began as a Mechanical Engineer. In the third year of my studies, I was able to choose a major, and I chose Mechatronics &amp; Robotics. In addition to my hobby, I enjoy front-end web development (hence the existence of this website 😉).</p>
-            <p> ✨ 4th year mechatronics &amp; robotics student at the HvA ✨. In my free time I like to go bouldering and running (mostly 5 of 10km). 🏃‍💨 </p>
-            <p> Currently I'm interested in 🦾robotics🦾 combined with 🧠computer vision / AI🧠.</p>
+        <div className="home_about">
+            <div className="about_text">
+                <h2> Me, Myself &amp; I</h2>
+                <p> Although I am studying mechatronics &amp; robotics, I began as a Mechanical Engineer. In the third year of my studies, I was able to choose a major, and I chose Mechatronics &amp; Robotics. In addition to my hobby, I enjoy front-end web development (hence the existence of this website 😉).</p>
+                <p> ✨ 4th year mechatronics &amp; robotics student at the HvA ✨. In my free time I like to go bouldering and running (mostly 5 of 10km). 🏃‍💨 </p>
+                <p> Currently I'm interested in 🦾robotics🦾 combined with 🧠computer vision / AI🧠.</p>
+            </div>
         </div>
-    </div>
     )
 }
